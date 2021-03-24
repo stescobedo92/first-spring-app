3f6a194b167a13971da2de92cb79c64cf9d7a2b1
stescobedo92 <stescobedo.31@gmail.com>
2021-03-23 23:15:51 -0400

    Added the greetig class

---
+++ src/main/java/first/spring/firstspringapp/Greeting.java
@@ -0,0 +1,24 @@
+package first.spring.firstspringapp;
+
+
+public class Greeting {
+    private Long id;
+    private String message;
+
+    public Greeting(Long id, String message){
+        this.id = id;
+        this.message = message;
+    }
+
+    public Long getId(){ return id; }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getMessage() { return message; }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+}
