1a64ed8a1b00aaaa7dd2261c97d11e56e28e2c2b
stescobedo92 <stescobedo.31@gmail.com>
2021-03-23 23:16:13 -0400

    Added the greetig controller class

---
+++ src/main/java/first/spring/firstspringapp/GreetingController.java
@@ -0,0 +1,12 @@
+package first.spring.firstspringapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "greeting")
+public class GreetingController {
+
+    //M
+
+}
